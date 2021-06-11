856b1962582ab71d7655e05f4e62f10e2c10750f
pp8991 <prakharpandey96@gmail.com>
2021-06-11 17:13:14 +0530

    Adding FrontEnd

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URI = 'mongodb://localhost:27017/urlshortener';
+const DB_URI = 'mongodb://127.0.0.1:27017/urlshortener';
 
 mongoose.connect(DB_URI ,{
     useNewUrlParser: true,
